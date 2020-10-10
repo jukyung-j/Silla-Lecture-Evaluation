0b344e1d2fff8299df6623f1b735a447bb6b0edf
jukyung-j <wjdwnrud123@naver.com>
2020-10-10 23:15:16 +0900

    fail login

---
+++ WebContent/view/index.jsp
@@ -0,0 +1,45 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>로그인</title>
+	<style>
+		table{
+			text-align:right;
+			padding:10px;
+		}
+		body{
+			padding-top:200px;
+			text-align:center;
+			width:50%;
+			margin:0 auto; 
+			
+		}
+	</style>
+</head>
+<body align="center">
+<c:if test="${message == false }" >
+	<script>
+		alert("아이디나 비밀번호를 다시 입력해주세요");
+	</script>
+</c:if>
+	<h1>로그인 페이지</h1><hr>
+	<form action='${pageContext.request.contextPath}/sillauv/lecture-evaluation?action=login' method="POST">
+		<table align="center">
+			<tr>
+				<td>아이디:</td><td><input type="text" name="id" /></td>
+			</tr>
+			<tr>
+				<td><br>비밀번호:</td><td><br><input type="password" name="pwd" /></td>
+			</tr>
+			<tr>
+				<td><br><input type="checkbox" name="자동로그인" value="auto-login">자동로그인</td>
+				<td><br><input type="submit" value="로그인" /></td>
+				<td><br><input type="button" value="회원가입 " onclick="location.href='join_form'"/></td>
+			</tr>
+		</table>
+	</form>
+</body>
+</html>
---
+++ src/com/sillauv_lecture_evaluation/LoginControl.java
@@ -0,0 +1,117 @@
+package com.sillauv_lecture_evaluation;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+/**
+ * Servlet implementation class LecControl
+ */
+@WebServlet("/sillauv/lecture-evaluation/*")
+public class LoginControl extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public LoginControl() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		// step #1. get request parameters
+		request.setCharacterEncoding("UTF-8");
+		HttpSession session = request.getSession();
+		String pathInfo = request.getPathInfo();
+		String action = request.getParameter("action");
+		String viewName = null;
+		MemberDO member = new MemberDO(); 
+		SILLADAO dao = (SILLADAO)session.getAttribute("dao");
+
+		if(dao == null) {
+			ServletContext context = getServletContext();
+			dao = new SillaDBCPDAO(
+					context.getInitParameter("dbcp_resource_name")
+			);
+			session.setAttribute("dao", dao);
+		}
+		
+		// step #2. data processing
+		// get routing info.
+		
+		if(pathInfo != null && pathInfo.length() > 1) {
+			if(pathInfo.equals("/index")) {
+				viewName = "/view/index.jsp";
+			}
+		}
+		else  {
+			viewName="/view/index.jsp";
+		}
+		if(action != null){
+			if(action.equals("login")) {		// 로그인 화면
+				String id = request.getParameter("id");
+				String pwd = request.getParameter("pwd");
+				boolean message = true;
+				request.setAttribute("message", message);
+				
+				try {
+					member = dao.Login(id);
+					if(member != null) {
+						if(member.getPwd().equals(pwd)) {	// 로그인 성공
+							message = true;
+							request.setAttribute("message", message);
+							viewName="/sillauv/lecture-evaluation/index";
+						}
+						else {		// 비밀번호 잘못 입력
+							message = false;
+							request.setAttribute("message", message);
+							viewName="redirect:/sillauv/lecture-evaluation/index";
+						}
+					}
+					else {			// 아이디 잘못 입력
+						message = false;
+						request.setAttribute("message", message);
+						viewName="redirect:/sillauv/lecture-evaluation/index";
+					}
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+			}
+		}
+		if(viewName != null) {
+			if(viewName.contains("redirect:")) {
+				String location = viewName.split(":")[1];
+				response.sendRedirect(request.getContextPath()+location);
+			}
+			else{
+				RequestDispatcher view = request.getRequestDispatcher(viewName);
+				view.forward(request, response);
+			}
+		}
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
